1508635bdb9115bc5d7b3fc0c90a031b4174f36c
brechtvandesijpe <60568024+brechtvandesijpe@users.noreply.github.com>
2025-09-16 15:59:48 +0200

    Initial commit

---
+++ src/main/java/be/breroeluean/HolySwimmingMod.java
@@ -0,0 +1,34 @@
+package be.breroeluean;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.minecraft.world.World;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.EntityPose;
+
+public class HolySwimmingMod implements ModInitializer {
+    public static final String MOD_ID = "holyswimmingmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (client.world != null) {
+                disableSwimming(client.world);
+            }
+        });
+    }
+
+    private static void disableSwimming(World world) {
+        for (PlayerEntity player : world.getPlayers()) {
+            if (player.isSwimming()) {
+                player.setSwimming(false);
+                player.setPose(EntityPose.STANDING);
+            }
+        }
+    }
+}
