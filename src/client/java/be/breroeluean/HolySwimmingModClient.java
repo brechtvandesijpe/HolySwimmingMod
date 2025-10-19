f57fa2529a563d9d6c96f5c53c7fc34e96ce1cde
brechtvandesijpe <brecht.van.de.sijpe@gmail.com>
2025-10-19 18:21:50 +0200

    Split server & client + Move to client

--- src/client/java/be/breroeluean/HolySwimmingModClient.java
+++ src/client/java/be/breroeluean/HolySwimmingModClient.java
@@ -1,10 +1,28 @@
 package be.breroeluean;
 
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.api.ClientModInitializer;
+
+import net.minecraft.entity.EntityPose;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
 
 public class HolySwimmingModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (client.world != null) {
+                disableSwimming(client.world);
+            }
+        });
 	}
+
+    private static void disableSwimming(World world) {
+        for (PlayerEntity player : world.getPlayers()) {
+            if (player.isSwimming()) {
+                player.setSwimming(false);
+                player.setPose(EntityPose.STANDING);
+            }
+        }
+    }
 }
--- src/main/java/be/breroeluean/HolySwimmingMod.java
+++ src/main/java/be/breroeluean/HolySwimmingMod.java
@@ -1,14 +1,10 @@
 package be.breroeluean;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+//
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import net.minecraft.world.World;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.EntityPose;
 
 public class HolySwimmingMod implements ModInitializer {
     public static final String MOD_ID = "HolySwimmingMod";
@@ -16,19 +12,6 @@ public class HolySwimmingMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (client.world != null) {
-                disableSwimming(client.world);
-            }
-        });
-    }
 
-    private static void disableSwimming(World world) {
-        for (PlayerEntity player : world.getPlayers()) {
-            if (player.isSwimming()) {
-                player.setSwimming(false);
-                player.setPose(EntityPose.STANDING);
-            }
-        }
     }
 }
